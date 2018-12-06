2e0ebab917119721356702989c9bc8b4a2ce498c
HeresJohnny5 <ericksonjohn01@hotmail.com>
2018-12-05 20:20:57 -0500

    Add IBOutlet for dateLabel and IBAction for didChangeDate function.

--- datePickeriOS/ViewController.swift
+++ datePickeriOS/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var dateLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    dateLabel.text = "Choose a date"
   }
 
-
+  @IBAction func didChangeDate(_ sender: UIDatePicker) {
+    let date: Date = sender.date
+    let formatter: DateFormatter = DateFormatter()
+    formatter.dateFormat = "EEEE"
+    
+    let dayOfWeek: String = formatter.string(from: date)
+    
+    dateLabel.text = "The day is a \(dayOfWeek)."
+  }
 }
-
